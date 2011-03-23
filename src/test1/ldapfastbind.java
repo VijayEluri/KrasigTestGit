93ee8a7f10d5f1c405e80c5de661ea3fd34a1e99
KrasiG <krasig@gmail.com>
2011-03-23 15:20:01 +0200

    555

--- src/test1/ldapfastbind.java
+++ src/test1/ldapfastbind.java
@@ -16,7 +16,7 @@ import javax.naming.ldap.LdapContext;
 
 class FastBindConnectionControl implements Control {
 	/**
-	 * aaaaaaaaaa bbbbbb cccccccccc 2 555
+	 * aaaaaaaaaa bbbbbb cccccccccc 2 333 555
 	 */
 	private static final long serialVersionUID = -6816941722609631483L;
 	public byte[] getEncodedValue() {
